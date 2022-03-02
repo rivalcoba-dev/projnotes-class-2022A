3ae9e4c58951bdf7c0b6b89110e701a470df8721
rivalcoba <ivan.rivalcoba@gmail.com>
2022-03-02 07:53:08 -0600

    Adding Babel-watch

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function (req, res) {
   // render manda a renderizar (generar y entregar)
   // la vista al cliente
   // Calculando
-  let emojieDataset = ['💻', '👨‍💻', '🎈', '🎄','🦺','🎢','🎁'];
+  let emojieDataset = ['💻', '👨‍💻', '🎈', '🎄','🦺','🎢','🎁','🚆','🌍','♥'];
   let emojie = emojieDataset[Math.floor(Math.random() * emojieDataset.length)];
   res.render('index',
     // Este es el View-Model
