ad26b16d078c7aad6a7cfc24e0520eafc30f0002
rivalcoba <ivan.rivalcoba@gmail.com>
2022-03-30 07:55:50 -0600

    Testing de ES06 en Front-end

--- client/index.js
+++ client/index.js
@@ -1,1 +1,24 @@
 console.log("🎁 Webpack Working!!!");
+
+// Default parameters ES6/2015
+let show = (m = "😝") => {
+  console.log(m);
+};
+show();
+
+// Promises ES06
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("function resolve");
+    }, 10000);
+  });
+}
+
+async function asyncCall() {
+  console.log("Calling asyn function!!!");
+  const result = await resolveAfter2Seconds();
+  console.log(result); // Imprime "function resolve" en la consola
+}
+
+asyncCall();
