d904d29af92c608176898dcfa38125d534de8921
rivalcoba <ivan.rivalcoba@gmail.com>
2022-04-08 07:56:11 -0500

    Agregando Eslint al proyecto Aun no integrado a Babel

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,6 +3,8 @@
 const path = require("path");
 // Plugins para Webpack
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// Agregando soporte para ESLINT
+const EslintWebpackPlugin = require('eslint-webpack-plugin'); 
 
 module.exports = {
   // 1. Especificar el archivo de entrada
@@ -67,5 +69,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: path.join("stylesheets", "styles.css"),
     }),
+    new EslintWebpackPlugin()
   ],
 };
