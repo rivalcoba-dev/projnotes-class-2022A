d477197898a6787abc736c0d6565558f40f6007f
rivalcoba <ivan.rivalcoba@gmail.com>
2022-05-18 08:25:54 -0500

    S38 Barra de navegacion movil

--- client/index.js
+++ client/index.js
@@ -1,30 +1,14 @@
 /* eslint-disable no-console */
+/* global M */
 
 // Incorporando estilos
 // a mi bundle
 import './styles/mystyle.css';
 
-console.log('🎁 Front-End Working!!!');
-
-// Default parameters ES6/2015
-const show = (m = '😝') => {
-  console.log(m);
-};
-show();
-
-// Promises ES06
-function resolveAfter2Seconds() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('function resolve');
-    }, 10000);
-  });
-}
-
-async function asyncCall() {
-  console.log('Calling async function!!!');
-  const result = await resolveAfter2Seconds();
-  console.log(result); // Imprime "function resolve" en la consola
-}
-
-asyncCall();
+/* Inicializando elementos de materializecss */
+document.addEventListener('DOMContentLoaded', () => {
+  // Obteniendo la referencia a la barra de navegacion
+  // lateral
+  const sideNavs = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sideNavs);
+});
