8e2be6e6a6eeb2eadf273e93208a6cfd15d5a42e
rivalcoba <ivan.rivalcoba@gmail.com>
2022-05-25 08:38:40 -0500

    S41 Agregando Menu Dropdown De acciones

--- client/index.js
+++ client/index.js
@@ -7,8 +7,13 @@ import './styles/mystyle.css';
 
 /* Inicializando elementos de materializecss */
 document.addEventListener('DOMContentLoaded', () => {
+  /* Inicializando los sideNavas */
   // Obteniendo la referencia a la barra de navegacion
   // lateral
   const sideNavs = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sideNavs);
+
+  /* Inicializando los dropdown */
+  const dropdowns = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(dropdowns);
 });
