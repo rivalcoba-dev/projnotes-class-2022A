5242eb9651b27125bfb4a49f3e47765dfbf6c72b
rivalcoba <ivan.rivalcoba@gmail.com>
2022-06-02 14:22:11 -0500

    Contectando la base usando variables de entorno

--- server/config/connectToDatabse.js
+++ server/config/connectToDatabse.js
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
+import configKeys from './configKeys';
+
+const {MONGO_PORT} = configKeys;
+const {MONGO_USER} = configKeys;
+const {MONGO_PASSWORD} = configKeys;
+const {MONGO_IP} = configKeys;
 
 export default () => {
   // Conectando la base de datos
-  mongoose.connect('mongodb://projnotes:123123@mongo:27017/?authSource=admin').then(() => {
+  mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+  .then(() => {
     console.log('Conexion exitosa a la base de datos');
   }).catch((error)=>{
     console.log(`Error when connecting database: ${error}`);
