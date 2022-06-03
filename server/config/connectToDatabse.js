815e57b7c8836dd85c9c007e2daf8921b20ce0a1
rivalcoba <ivan.rivalcoba@gmail.com>
2022-06-02 19:38:48 -0500

    Conectando Base de datos on Retry

--- server/config/connectToDatabse.js
+++ server/config/connectToDatabse.js
@@ -7,11 +7,16 @@ const {MONGO_PASSWORD} = configKeys;
 const {MONGO_IP} = configKeys;
 
 export default () => {
-  // Conectando la base de datos
-  mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
-  .then(() => {
-    console.log('Conexion exitosa a la base de datos');
-  }).catch((error)=>{
-    console.log(`Error when connecting database: ${error}`);
-  });
+  function connectWithRetry(){
+    // Conectando la base de datos
+    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+    .then(() => {
+      console.log('Conexion exitosa a la base de datos');
+    }).catch((error)=>{
+      console.log(`Error when connecting database: ${error}`);
+      console.log(`Retrying connection`);
+      setTimeout(connectWithRetry, 5000);
+    });
+  }
+  connectWithRetry();
 }
