f85ebf1e9ef237e4beba3b20fc4273f5f0e0125e
rivalcoba <ivan.rivalcoba@gmail.com>
2022-06-06 08:41:06 -0500

    S47 Validacion BackFront AddProject

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -18,20 +18,40 @@ const add = (req, res) => {
 // Procesa el formulario que Agrega ideas de proyectos
 // POST /projects/add
 const addPost = (req, res) => {
-  const { errorData: error } = req;
-  if (error) {
+  const { errorData } = req;
+  // Crear view models para este actio method
+  let project = {};
+  let errorModel = {};
+  if (errorData) {
     log.info('Se retorna objeto de error de validacion');
+    // Rescantado el objeto validado
+    project = errorData.value;
+    // Usamos reduce para generar un objeto
+    // de errores a partir de inner
+    errorModel = errorData.inner.reduce((prev, curr) => {
+      // Creamos una vaiabre temporal para evitar
+      // el error "no-param-reassign" el cual me
+      // exorta a evitar reasignar los valores de
+      // los argumentos una funcion
+      const newVal = prev;
+      newVal[`${curr.path}Error`] = curr.message;
+      return newVal;
+    }, {});
     // La validacion fallo
-    res.status(200).json(error);
+    // res.status(200).json(errorData);
   } else {
-    // Desestructurando la informacion
-    // del formulario
-    const { validData: project } = req;
     log.info('Se retorna objeto Projecto valido');
+    // Desestructurando la informacion
+    // del formulario del objeto valido
+    const { validData } = req;
     // Regresar un objeto con los datos
     // obtenidos del formulario
-    res.status(200).json(project);
+    // res.status(200).json(validData);
+    project = validData;
   }
+  // Respondemos con los viewModels generados
+  res.render('projects/addProjectView', { project, errorModel });
+  // res.status(200).json({ project, errorModel });
 };
 
 // Exportando el controlador
